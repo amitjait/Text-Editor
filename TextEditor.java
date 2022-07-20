0fb96738388d1c7d5d4c5a294ca08084c8aa8038
amitjait <amit.rimit591@gmail.com>
2022-07-21 01:26:29 +0530

    updating

--- TextEditor.java
+++ TextEditor.java
@@ -1,43 +1,58 @@
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-import java.awt.*;
 
-import javax.swing.*;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.JTextArea;
 
 public class TextEditor extends JFrame implements ActionListener {
 
 
     JTextArea textArea;
     JScrollPane scrollPane;
+    JPanel utilityPanel;
     JLabel fontLabel; 
     JSpinner fontSizeSpinner;
     JButton fontColorButton;
     JComboBox fontBox;
     JMenuBar menuBar;
-    JMenu fileMenu;
-    JMenuItem openItem;
-    JMenuItem saveItem;
-    JMenuItem exitItem;
-
-
+    JMenu fileMenu, editMenu, themeMenu;
+    JMenuItem openItem, saveItem, exitItem, newitem, cutItem, pasteItem, copyItem, selectAllItem, darkThemeItem, lightThemeItem, cutomeThemeItem; 
+    
     TextEditor(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Text Editor");
-        this.setSize(500, 500);
+        this.setSize(1100, 700);
         this.setLocationRelativeTo(null); 
-        this.setLayout(new FlowLayout());  
+        this.setLayout(new FlowLayout()); 
+        this.setResizable(false);
+        
+        // this.pack();
         
         textArea = new JTextArea();
         // textArea.setPreferredSize(new Dimension(450, 450));
@@ -45,9 +60,11 @@ public class TextEditor extends JFrame implements ActionListener {
         textArea.setWrapStyleWord(true);
         textArea.setFont(new Font("Arial", Font.PLAIN, 20));
 
+
         scrollPane = new JScrollPane(textArea);
-        scrollPane.setPreferredSize(new Dimension(4500, 4500));
-        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setPreferredSize(new Dimension(1080, 590));
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
         fontLabel = new JLabel("Font: ");
 
@@ -75,12 +92,20 @@ public class TextEditor extends JFrame implements ActionListener {
         fontBox.addActionListener(this);
         fontBox.setSelectedItem("Arial");
 
+        utilityPanel = new JPanel();
+        utilityPanel.add(fontLabel);
+        utilityPanel.add(fontSizeSpinner);
+        utilityPanel.add(fontColorButton);
+        utilityPanel.add(fontBox);
+
         // ---MenuBar----
 
         menuBar = new JMenuBar();
+
         fileMenu = new JMenu("File");
         openItem = new JMenuItem("Open");
         saveItem = new JMenuItem("Save");
+        newitem = new JMenuItem("New File");
         exitItem = new JMenuItem("Exit");
 
 
@@ -90,9 +115,43 @@ public class TextEditor extends JFrame implements ActionListener {
 
         fileMenu.add(openItem);
         fileMenu.add(saveItem);
+        fileMenu.add(newitem);
         fileMenu.add(exitItem);
 
+
+        // ------Edit menu-------
+
+        editMenu = new JMenu("Edit");
+        cutItem = new JMenuItem("Cut");
+        pasteItem = new JMenuItem("Paste");
+        selectAllItem = new JMenuItem("Select All");
+        copyItem = new JMenuItem("Copy");
+
+        editMenu.add(cutItem);
+        editMenu.add(pasteItem);
+        editMenu.add(selectAllItem);
+        editMenu.add(copyItem);
+
+
+        // ------Edit menu-------
+
+        // -----Theme menu -----
+
+        themeMenu = new JMenu("Theme");
+        darkThemeItem = new JMenuItem("Dark Theme");
+        lightThemeItem = new JMenuItem("Light Theme");
+        cutomeThemeItem = new JMenuItem("Custome theme");
+
+        themeMenu.add(darkThemeItem);
+        themeMenu.add(lightThemeItem);
+        themeMenu.add(cutomeThemeItem);
+
+        // ----Theme Menu------
+
+
         menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(themeMenu);
 
 
 
@@ -100,14 +159,16 @@ public class TextEditor extends JFrame implements ActionListener {
         
 
         this.setJMenuBar(menuBar);
-        this.add(fontLabel);
-        this.add(fontSizeSpinner);
-        this.add(fontColorButton);
-        this.add(fontBox);
-        this.add(scrollPane);
+        this.add(utilityPanel);
+        // this.add(fontLabel);
+        // this.add(fontSizeSpinner);
+        // this.add(fontColorButton);
+        // this.add(fontBox);
+        
 
         
         // this.add(textArea);
+        this.add(scrollPane);
         this.setVisible(true);
     }
 
